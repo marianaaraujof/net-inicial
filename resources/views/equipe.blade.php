3a863d634f3daba8db0df405251623df7eff60a4
Paula Torres <paulabastostorres@gmail.com>
2018-07-12 13:47:03 -0300

    organização básica da pag. Equipe

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -3,25 +3,41 @@
 
 <div class="container-fluid">    
  		<div class="row">
- 			<div class="col-sm-4">
+ 			<div class="col-md-4">
  			</div>
- 			<div class="col-sm-4">
+ 			<div class="col-md-4">
  				<h1 class="text-center">Equipe</h1>
  				<br />
                 <br />
                 <br />
- 				<h3 class="text-center">Orientadores</h3> 
-                 <!-- Foto e contato de Pierre e Dani-->
+ 				<h2 class="text-center"><b>Orientadores</b></h2> 
+                 <div class="col-md-2">
+                <br />
+                    <h3 class="text-center">Pierre</h3> 
+                     <!-- Foto e contato de Pierre -->
+                <br />
+                    <h3 class="text-center">Danieli</h3> 
+                     <!-- Foto e contato de Dani-->
                 <br />
                 <br />
+
+            <h2 class="text-center"><b>Bolsistas Colaboradores</b></h2> 
                 <br />
-            <h3 class="text-center">Bolsistas Colaboradores</h3> 
-                 <!-- Foto e contato de todos-->
+                    <h3 class="text-center">Mariana</h3>
+                    <!-- Foto e contato-->
                 <br />
+                    <h3 class="text-center">Matheus Oliveira</h3>
+                    <!-- Foto e contato-->
+                <br />
+                    <h3 class="text-center">Paula Torres</h3>
+                    <!-- Foto e contato-->
+                <br />
+                    <h3 class="text-center">Hércules Pedrosa</h3>
+                    <!-- Foto e contato-->
                 <br />
                 <br />
  			</div>
  		</div>
  	</div>
 
-@endsection
+@endsection 

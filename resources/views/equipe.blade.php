7a4a170434ca12a2ca8821ef9c35eba7859a0fc9
Paula Torres <paulabastostorres@gmail.com>
2018-07-12 12:13:21 -0300

    correcao dos nomes do menu

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -1,7 +1,15 @@
 @extends('templates.template1')
 @section('eplais')
 
-<h1> Nossa equipe </h1>
-
+<div class="container-fluid">    
+ 		<div class="row">
+ 			<div class="col-sm-4">
+ 				<h1 class="text-center">Nossa Equipe</h1>
+ 				<br />
+ 				<h3 class="text-center">Orientadores</h3> 
+                <h3 class="text-center">Bolsistas Colaboradores</h3> 
+            </div>
+ 		</div>
+ 	</div>
 
 @endsection

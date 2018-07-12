45add839cf207d6f6695bd004f0dfd1a5ba2be5d
Paula Torres <paulabastostorres@gmail.com>
2018-07-12 13:24:02 -0300

    Tentativa de organizar a página da equipe

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -4,11 +4,23 @@
 <div class="container-fluid">    
  		<div class="row">
  			<div class="col-sm-4">
- 				<h1 class="text-center">Nossa Equipe</h1>
+ 			</div>
+ 			<div class="col-sm-4">
+ 				<h1 class="text-center">Equipe</h1>
  				<br />
+                <br />
+                <br />
  				<h3 class="text-center">Orientadores</h3> 
-                <h3 class="text-center">Bolsistas Colaboradores</h3> 
-            </div>
+                 <!-- Foto e contato de Pierre e Dani-->
+                <br />
+                <br />
+                <br />
+            <h3 class="text-center">Bolsistas Colaboradores</h3> 
+                 <!-- Foto e contato de todos-->
+                <br />
+                <br />
+                <br />
+ 			</div>
  		</div>
  	</div>
 

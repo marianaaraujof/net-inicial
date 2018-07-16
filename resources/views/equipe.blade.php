5cdd93278dfa8adbad18b30f771598ef254316ce
Paula Torres <paulabastostorres@gmail.com>
2018-07-16 08:40:34 -0300

    tirando os links do bootstrap da equipe.blade.php

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -1,17 +1,4 @@
-<!DOCTYPE html>
- <html lang="pt-br">
 
- <head>
- 	<meta charset="utf-8"/>
- 	<title>SisPag</title>
- 	<meta charset="utf-8">
- 	<meta name="viewport" content="width=device-width, initial-scale=1">
- 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
- 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
- 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
- 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
- </head>
- <body>
 @extends('templates.template1')
 @section('eplais')
 
@@ -56,4 +43,3 @@
 
 @endsection 
 
-</body>

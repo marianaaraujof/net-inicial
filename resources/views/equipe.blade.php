be269ed4549a43742ef20790655b31e537fca451
Paula Torres <paulabastostorres@gmail.com>
2018-07-16 08:38:22 -0300

    adicionando os links do bootstrap

--- resources/views/equipe.blade.php
+++ resources/views/equipe.blade.php
@@ -1,3 +1,17 @@
+<!DOCTYPE html>
+ <html lang="pt-br">
+
+ <head>
+ 	<meta charset="utf-8"/>
+ 	<title>SisPag</title>
+ 	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+ 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+ 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+ </head>
+ <body>
 @extends('templates.template1')
 @section('eplais')
 
@@ -23,7 +37,7 @@
 
             <h2 class="text-center"><b>Bolsistas Colaboradores</b></h2> 
                 <br />
-                    <h3 class="text-center">Mariana</h3>
+                    <h3 class="text-center">Mariana Figueiredo</h3>
                     <!-- Foto e contato-->
                 <br />
                     <h3 class="text-center">Matheus Oliveira</h3>
@@ -41,3 +55,5 @@
  	</div>
 
 @endsection 
+
+</body>

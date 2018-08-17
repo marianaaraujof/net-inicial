49caf727ee16c8e484ec04d3b225ec829bf728bc
Mariana Araujo <marianaaraujof@ufrn.edu.br>
2018-08-17 13:19:08 -0300

    nav bar

--- resources/views/templates/template1.blade.php
+++ resources/views/templates/template1.blade.php
@@ -65,6 +65,33 @@
         </style>
 </head>
 <body>
+<nav class="navbar navbar-expand-lg navbar-light bg-light sticky-top">
+    <a class="navbar-brand justify-content-start" href="#">
+        <img src="#" width="30" height="30" alt="">
+    </a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+    <ul class="navbar-nav">
+        <li class="nav-item">
+            <a class="nav-link" href="/">Início</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/equipe">Equipe</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/projetos">Projetos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/"></a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="/"></a>
+        </li>
+    </ul>
+    </div>
+</nav>
     @yield('eplais')
 </body>
 </html>

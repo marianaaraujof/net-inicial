318efd048e99cd417f269284c72456c2eb391b7f
Paula Torres <paulatorres@iMac-de-LAIS-EP-7.local>
2018-08-27 16:36:08 -0300

    background welcome estrelas e nuvem

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,22 +2,29 @@
 @section('cfg_title_path_0', 'Inicio')
 @section('cfg_section_geral')
 
-<body id="welcome">
+<body id="welcome" class="bgCor bgEstrelas">
 <div>
     <div id="main-header">
+    <br />
+        <div class="box-logo">
+            <img src="{{asset('imgs/EP_logo_01.svg')}}"> </img>
+        </div>
+        <br />
         <h1 id="frase"> Do nada ao <br /> infinito! </h1>
-        <h1> Escola de programacao do lais </h1>
+        <br />
+        <div>
+            <h1 id="frase2"> De onde, </h1>
+            <h1 id="frase3"> e para onde? <h1>
+        </div>
     </div>
 
-    <span class="bg-cor"></span>
-    <span class="bg-estrelas"></span>
 
-    <div class="box-video">
+    <div class="box-video bgNuvem">
         <video src="{{asset('video/EscolaDeProgramacao.mp4')}}" controls>
         </video>
     </div> 
 </div>
-<body>
+</body>
 @endsection
 
 

b0bcae39cb956a0dabd855b0d54df47f170b9a23
Paula Torres <paulatorres@iMac-de-LAIS-EP-7.local>
2018-08-28 14:01:21 -0300

    backgroun welcome terra adc

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,23 +3,24 @@
 @section('cfg_section_geral')
 
 <body id="welcome" class="bgCor bgEstrelas">
-<div id="bgFaixas">
-    <div id="main-header">
+<div class="bgFaixasDown">
+    <div>
+        <div id="main-header">
 
-        <div class="box-logo">
-            <img src="{{asset('imgs/EP_logo_01.svg')}}"> </img>
-        </div>
-        
+            <div class="box-logo">
+                <img src="{{asset('imgs/EP_logo_01.svg')}}"> </img>
+            </div>
+            
         <h1 id="frase"> Do nada ao <br /> infinito! </h1>
-        
-        <div>
-            <h1 id="frase2"> De onde, </h1>
-            <h1 id="frase3"> e para onde? <h1>
+            
+            <div>
+                <h1 id="frase2"> De onde, </h1>
+                <h1 id="frase3"> e para onde? <h1>
+            </div>
         </div>
     </div>
 
-
-    <div class="box-video bgNuvem">
+    <div id="video" class="box-video">
         <video src="{{asset('video/EscolaDeProgramacao.mp4')}}" controls>
         </video>
     </div> 

3b44970769e43bb7d26f6bc50556a8f779da0c84
Paula Torres <paulatorres@iMac-de-LAIS-EP-7.local>
2018-08-28 09:41:46 -0300

    faixas adicionadas ao background welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,15 +3,15 @@
 @section('cfg_section_geral')
 
 <body id="welcome" class="bgCor bgEstrelas">
-<div>
+<div id="bgFaixas">
     <div id="main-header">
-    <br />
+
         <div class="box-logo">
             <img src="{{asset('imgs/EP_logo_01.svg')}}"> </img>
         </div>
-        <br />
+        
         <h1 id="frase"> Do nada ao <br /> infinito! </h1>
-        <br />
+        
         <div>
             <h1 id="frase2"> De onde, </h1>
             <h1 id="frase3"> e para onde? <h1>

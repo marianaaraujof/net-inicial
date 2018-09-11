923dfa0629c4082bdc9d844495831e86d178ac2a
Paula Torres <paulatorres@iMac-de-LAIS-EP-7.local>
2018-09-11 14:00:06 -0300

    correcao da nuvem da welcome, responsividade

---
+++ resources/views/templates/menu-preto.blade.php
@@ -0,0 +1,22 @@
+
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top mr-2" style="height: 120px; width:100%;">
+        <a class="navbar-brand justify-content-start" href="#">
+            <img src="{{ asset('imgs/Faixas_coloridas.svg') }}" alt="faixas">
+        </a>
+        <!-- <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button> -->
+        <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul class="navbar-nav">
+            <li class="nav-item nav-estilo">
+                <a class="nav-link" href="/">Início</a>
+            </li>
+            <li class="nav-item nav-estilo">
+                <a class="nav-link" href="/equipe">Equipe</a>
+            </li>
+            <li class="nav-item nav-estilo">
+                <a class="nav-link" href="/projetos">Projetos</a>
+            </li>
+        </ul>
+        </div>
+        </nav>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,9 +1,10 @@
-@extends('templates/layout')
+@extends('templates/layoutWelcome')
 @section('cfg_title_path_0', 'Inicio')
 @section('cfg_section_geral')
 
 <body class="bgCor bgEstrelas">
 <div class="bgFaixasDown">
+<div class="bgLua">
     <div>
         <div id="main-header">
 
@@ -24,17 +25,65 @@
         <video src="{{asset('video/EscolaDeProgramacao.mp4')}}" controls>
         </video>
     </div> 
-    <div id="welcome">
-        <div class="frases">
-        <p> "um pouquinho de texto" </p>
-        <p> "um pouquinho de texto" </p>
-        <p> "um pouquinho de texto" </p>
-        </div>
+</div>
+    <div id="welcome" class="frasesInsp">
+            <div class="media">
+                    <img src="{{asset('imgs/avatar.png')}}" class="media-object" style="width:60px">
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading"></h4>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p class=autores>(Lorem ipsum)</p>
+            </div>
+
+            <div class="media">
+            <img src="{{asset('imgs/avatar.png')}}" class="media-object" style="width:60px">
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading"></h4>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p class=autores>(Lorem ipsum)</p>
+            </div>
+
+            <div class="media">
+                    <img src="{{asset('imgs/avatar.png')}}" class="media-object" style="width:60px">
+            </div>
+            <div class="media-body">
+                <h4 class="media-heading"></h4>
+                <p> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+                <p class=autores>(Lorem ipsum)</p>
+            </div>
+            <div class="flex-container">
+                <h1 class="pbl"><b>Problem <br /> Based <br /> Learning</b></h1> 
+                <div class="box-pbl pblNumeros pblEtapas">
+                    <div>
+                        <p>1</p>
+                        <h3>Hospital</h3>
+                    </div>
+                    <div>
+                        <p>2</p>
+                        <h3>Problema</h3>
+                    </div>    
+                    <div>
+                        <p>3</p>
+                        <h3>Tecnologia</h3>
+                    </div>
+                    <div>
+                        <p>4</p>
+                        <h3>Escola de Programação</h3>
+                    </div>
+                    <div>
+                        <p>5</p>
+                        <h3>Alunos</h3>
+                    </div>
+                    <div>    
+                        <p>6</p>
+                        <h3>Produto</h3>
+                    </div>
+                </div>
+            </div>
     </div>
 
 </div>
 </body>
 @endsection
-
-
-

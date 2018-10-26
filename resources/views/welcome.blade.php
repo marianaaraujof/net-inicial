6daaa4a193ffebd74bdcab3933a157a1802e8a52
Paula Torres <paulatorres@iMac-de-LAIS-EP-7.local>
2018-10-26 15:34:47 -0300

    oraginzacao frases welcome ok

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,40 +5,72 @@
 <body class="bgCor">
     <div class="bgEstrelas">
         <div >
-            <div class="bgLua">
+            <div class="bgLua"></div>
                 <div>
 
                     <section id="main">
 
                         <header class="mainHeader"> 
                     
-                                <img class="LogoEP" src="{{asset('imgs/EP_logo_01.svg')}}"> </img>
+                                <div class="LogoEP"> </div>
 
                         </header>
 
                         <article class="mainArticle"> 
                                 
-                                <h1 class="textosArticle-Slogan"> Do nada ao <br /> infinito! </h1>
+                                <h1 class="textosArticle-Slogan"> Do nada ao infinito! </h1>
 
                         </article>    
 
                         <aside class="mainAside"> 
 
-                                <h1 class="textosAside-Subtitulo"> De onde, </h1>
-                                <h1 class="textosAside-Subtitulo"> e para onde? </h1> 
+                                <h1 class="textosAside-Subtitulo"> De onde, <strong> e para onde? </strong></h1>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse feugiat lorem a lacus viverra feugiat. Etiam porta at erat vitae rhoncus. Maecenas non nisi luctus, interdum risus ac, condimentum ex. In imperdiet venenatis ipsum, et aliquet eros condimentum at. Aliquam accumsan sem mi. Ut maximus luctus suscipit. Etiam venenatis libero vitae bibendum vehicula. Ut luctus augue nec sapien feugiat, vitae scelerisque diam volutpat. Vestibulum elementum et mauris nec laoreet. Nullam hendrerit mi ligula, vel porta nibh interdum vitae. Suspendisse aliquam eu tortor at pulvinar. Fusce varius faucibus lectus id scelerisque. Nunc eu enim turpis. Etiam lacinia lacinia facilisis. Curabitur rutrum lobortis nisl. Ut ligula nisl, pellentesque nec maximus vitae, convallis ac arcu.</p>
 
                         </aside>
 
-                        <conteudo class="mainContainer">
+                        <article class="mainContainer">
                             
-                            <video class ="VideoContainer" src="{{asset('video/EscolaDeProgramacao.mp4')}}" controls>
+                            <div class="videoArticle">
 
-                        </conteudo>
+                                <video src="{{asset('video/EscolaDeProgramacao.mp4')}}" controls>
+                                
+                            </div>
+
+                        </article>
+
+                        <article class="conteudoFrases-Welcome">
+
+                                <div class="cardFrases-Welcome">
+                                    <img src="{{asset('imgs/avatar.png')}}" style="width:60px">
+                                    <div class="textoFrases-Welcome">
+                                        <h4>"Lorem ipsum dolor sit amet, consectetuer"</h4>
+                                        <p> (Lorem Ipsum) </p>
+                                    </div>
+                                </div>
+
+                                <div class="cardFrases-Welcome">
+                                    <img src="{{asset('imgs/avatar.png')}}" style="width:60px">
+                                    <div class="textoFrases-Welcome">
+                                        <h4>"Lorem ipsum dolor sit amet, consectetuer"</h4>
+                                        <p> (Lorem Ipsum) </p>
+                                    </div>
+                                </div>
+
+                                <div class="cardFrases-Welcome">
+                                    <img src="{{asset('imgs/avatar.png')}}" style="width:60px">
+                                    <div class="textoFrases-Welcome">
+                                        <h4>"Lorem ipsum dolor sit amet, consectetuer"</h4>
+                                        <p> (Lorem Ipsum) </p>
+                                    </div>
+                                </div>
+
+                        </article>
 
                     </section>
 
                 </div>
-            </div>
+            
         </div> 
     </div>       
 </body>
